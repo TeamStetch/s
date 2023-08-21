d105fa6a7b32ac52ec6d74315b06749a03813938
TeamStetch <130715050+TeamStetch@users.noreply.github.com>
2023-08-21 12:10:15 +0300

    Update thumbnails.py

--- Stetchfy/utils/thumbnails.py
+++ Stetchfy/utils/thumbnails.py
@@ -98,7 +98,7 @@ async def gen_thumb(videoid):
 
         draw.text(
             (600, 150),
-            "NIGHTOSPHERE",
+            "STETCHFY",
             fill="white",
             stroke_width=2,
             stroke_fill="white",
